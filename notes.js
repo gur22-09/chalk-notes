29e7455bc95f2ea64b3e26bce5d2986edc90e838
GURPRIT SINGH <gurprit.business94@gmail.com>
2020-04-30 22:39:37 +0530

    added loadNotes method

--- notes.js
+++ notes.js
@@ -1,50 +1,79 @@
 const fs = require('fs');
+const {Howl} = require('howler');
+const chalk = require('chalk');
 
+const log = console.log;
 
 const getNotes = ()=>('your damn notes...');
 
 
 const addNotes = (title,body)=>{
    const notes = loadNotes();
-   const duplicateNotes = notes.filter(el=>{
+   const duplicateNote = notes.find(el=>{
        notes.title === title;
    });
 
-   if(!duplicateNotes){
+   if(!duplicateNote){
     notes.push({
         title:title,
         body:body
     });
     errorAudio();
-    console.log('notes added');
+    log(chalk.green.inverse('notes added'));
    }else{
        
-       console.log('note already exixts!');
+    log(chalk.red.inverse('note already exixts!'));
       
    }
 
   
    saveNotes(notes);
+};
+
+const removeNotes = (title)=>{
+
+    const notes = loadNotes();
+
+    
+  
+     //filtering notes to remove the note required
+    const notesToKeep = notes.filter(note=>note.title !== title);
+     
+    
+    
+    if(notesToKeep.length !== notes.length){ //or you can check if notes.length > notesToKeep.length
+     saveNotes(notesToKeep);
+     log(chalk.green.inverse('note removed successfully'));
+    }else{
+     log(chalk.red.inverse('no note found!'))
+    }
+};
+
+const listNotes= () =>{
+    const notes = loadNotes();
+    log(chalk.magentaBright(`Your Notes`));
+
+    notes.forEach(note=>log(chalk.italic.inverse.yellow(note.title)));
 }
 
+const readNote = (title)=>{
+    const notes = loadNotes();
+
+    const noteToRead = notes.find(note=>note.title === title);
+
+    if(noteToRead){
+        log(chalk.inverse.cyan(noteToRead.title) +" : "+noteToRead.body );
+    }else{
+        log(chalk.red.inverse('No such note'));
+    }
+}
 
 function saveNotes(notes){
     const notesJSON = JSON.stringify(notes);
 
     fs.writeFileSync('notes.json',notesJSON);
-}
+};
 
-function errorAudio(){
-    let sound = new Howl({
-      src:['./utils/WINDOWS XP ERROR SOUND.wmv.mp3'],
-      autoplay:true,
-      loop:true,
-      volume:1,
-      
-    });
-
-    sound.play();
-}
 
 function loadNotes(){
     //using a try catch if in case there is not notes.json file already
@@ -56,10 +85,26 @@ function loadNotes(){
         return [];
     }
     
-} 
+} ;
+
+function errorAudio(){
+    let sound = new Howl({
+      src:['./utils/WINDOWS XP ERROR SOUND.wmv.mp3'],
+      autoplay:true,
+      loop:true,
+      volume:1,
+      
+    });
+
+    sound.play();
+};
+
 
 
 module.exports = {
     getNotes:getNotes,
-    addNotes:addNotes
-}
+    addNotes:addNotes,
+    removeNotes:removeNotes,
+    listNotes:listNotes,
+    readNote:readNote
+};
